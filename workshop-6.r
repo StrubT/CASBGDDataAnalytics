cd72114b62d0f1cee95a29c638ce783c8d83f9d8
Thomas Reto Strub <ths@strubt.ch>
2017-08-15 11:30:43 +0200

    add workshop 6

---
+++ workshop-6.r
@@ -0,0 +1,50 @@
+
+## INIT
+
+BANK_DF <- read.csv("bank-full.csv", sep = ";")
+
+NDX <- 1:nrow(BANK_DF)
+N <- 10000
+NDX_SMPL <- sample(NDX, N)
+BANK_DF <- BANK_DF[NDX_SMPL,]
+
+BANK_FORM <- y ~ .
+
+N <- nrow(BANK_DF)
+FOLD <- 10
+BANK_DF <- BANK_DF[sample(1:N, N),]
+SEGMENT <- trunc((0:(N - 1)) / N * FOLD) + 1
+BANK_DF <- data.frame(BANK_DF, SEGMENT)
+
+FNC_GET_PERFORMANCE <- function(DF, FORM, SEG) {
+
+	TRAIN <- subset(DF, SEGMENT != SEG)
+	TEST <- subset(DF, SEGMENT == SEG)
+
+	MODEL <- randomForest(FORM, TRAIN[, - ncol(TRAIN)])
+	PRED <- predict(MODEL, TEST[, - ncol(TEST)], type = "class")
+	TAB <- table(TEST$y, PRED)
+	RESULT <- sum(diag(TAB)) / sum(TAB)
+	return(RESULT)
+}
+
+library(randomForest)
+library(doParallel)
+
+## LOOP
+
+CORES <- detectCores() - 1
+print(paste("CORES:", CORES))
+
+registerDoParallel(cores = CORES)
+CL <- makeCluster(CORES)
+
+system.time({
+	RESULT <- foreach(I = 1:FOLD,
+										.combine = rbind,
+										.packages = "randomForest") %dopar%
+		FNC_GET_PERFORMANCE(BANK_DF, BANK_FORM, I)
+})
+mean(RESULT)
+
+stopCluster(CL)
